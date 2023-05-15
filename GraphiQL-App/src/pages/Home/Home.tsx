3e87a1b9875868c223309405fccf5b5eb995b146
SergEnergy <kis8883244@gmail.com>
2023-05-15 20:36:37 +0300

    del: delete package-lock and add to git ignore

--- GraphiQL-App/src/pages/Home/Home.tsx
+++ GraphiQL-App/src/pages/Home/Home.tsx
@@ -1,33 +1,39 @@
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../../helpers/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 import { useAppDispatch, useAppSelector } from '../../store/hooksRedux';
 import { exitUser, setUser } from '../../store/slices/userSlice';
 
 function Home() {
   const dispatch = useAppDispatch();
-  const [user] = useAuthState(auth);
   const navigate = useNavigate();
-  const data = useAppSelector((state) => state.userAuth);
+  const name = useAppSelector((state) => state.userAuth.name);
 
   useEffect(() => {
-    if (!user) {
-      dispatch(exitUser());
-      navigate('/login', { replace: true });
-    }
-    dispatch(
-      setUser({
-        email: user && user.email ? user.email : '',
-        id: user && user.uid ? user.uid : '',
-        token: user && user.refreshToken ? user.refreshToken : '',
-        name: '',
-      })
-    );
-  }, [user]);
+    const listenAuth = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        dispatch(exitUser());
+        navigate('/login', { replace: true });
+      }
+      if (user) {
+        dispatch(
+          setUser({
+            email: user && user.email ? user.email : '',
+            token: user ? user.refreshToken : '',
+            id: user ? user.uid : '',
+            name: name,
+          })
+        );
+      }
+    });
+
+    return () => {
+      listenAuth();
+    };
+  }, []);
 
   return <div className="margin-sticky">This is a home page</div>;
-  // className margin-sticky is required for sticky header - write it for parent element at Home
 }
 
 export default Home;
