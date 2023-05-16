44a5815585d5e224c6175748845fb7be7c01b11d
Kirill <bloodwingest@gmail.com>
2023-05-16 19:34:31 +0300

    feat: add login page translation

--- GraphiQL-App/src/pages/LoginPage/LoginPage.tsx
+++ GraphiQL-App/src/pages/LoginPage/LoginPage.tsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 import { Box, Container, Typography } from '@mui/material';
 
 import LogIn from '../../components/LogIn/LogIn';
+import { useTranslation } from 'react-i18next';
 
 function LoginPage() {
+  const { t, i18n } = useTranslation()
   return (
     <Container component="main" maxWidth="xl" className="margin-sticky">
       <Box
@@ -21,7 +23,7 @@ function LoginPage() {
         <LogIn />
         <Box sx={{ fontWeight: '500', fontSize: '1.2rem', width: '100%', mt: '20px' }}>
           <Typography variant="h6" component="p">
-            Don't have an account? <Link to="/register">Register now</Link>
+          {t('loginPage.haveNoAcc')} <Link to="/register">{t('loginPage.register')}</Link>
           </Typography>
         </Box>
       </Box>
