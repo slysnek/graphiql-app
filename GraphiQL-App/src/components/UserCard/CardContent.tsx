00f479d9fb34e6b38eaef539cfc7e8bae2c803c6
SergEnergy <kis8883244@gmail.com>
2023-05-20 21:41:34 +0300

    add: add animation flip card user

--- GraphiQL-App/src/components/UserCard/CardContent.tsx
+++ GraphiQL-App/src/components/UserCard/CardContent.tsx
@@ -1,10 +1,61 @@
 import './CardContent.css';
+import EMailImg from '/icons/EmLogo.png';
+import GHubImg from '/icons/GhLogo.png';
+import TGImg from '/icons/TgLogo.png';
 
-function CardContent() {
+interface CardContentProps {
+  onClick: () => void;
+  avatar: string;
+  name: string;
+  GHLink: string;
+  EMLink: string;
+  TGLink: string;
+}
+
+function CardContent(props: CardContentProps) {
+  const { onClick, avatar, name, GHLink, EMLink, TGLink } = props;
+
   return (
-    <div className="card">
-      <div className="card-back">Back</div>
-      <div className="card-front">Front</div>
+    <div className="author_card" onClick={onClick}>
+      <div className="author_card-back card-back">
+        <div className="card-back__title">My contacts</div>
+        <div className="card-back__contacts media-group">
+          <div className="media-group__block">
+            <div className="media-group__block_icon media-icon">
+              <img src={EMailImg} alt="Email" className="media-icon_img" />
+            </div>
+            <div className="media-group__block_name">{EMLink}</div>
+          </div>
+          <div className="media-group__block">
+            <div className="media-group__block_icon media-icon">
+              <img src={TGImg} alt="Telegram" className="media-icon_img" />
+            </div>
+            <div className="media-group__block_name">{TGLink}</div>
+          </div>
+          <div className="media-group__block">
+            <div className="media-group__block_icon media-icon">
+              <img src={GHubImg} alt="Github" className="media-icon_img" />
+            </div>
+            <div className="media-group__block_name">
+              <a className="media-group_link" href={GHLink} target="_blank" rel="noreferrer">
+                Link to github
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="author_card-front card">
+        <div className="card__line">
+          <div className="card__content">
+            <div className="card__content_name">{name}</div>
+            <div className="card__content_description">Junior Frontend Developer</div>
+            <div className="card__content_about">I am open to work! Click on card to find me!</div>
+          </div>
+        </div>
+        <div className="card__avatar">
+          <img src={avatar} alt="author avatar" className="card__avatar_img" />
+        </div>
+      </div>
     </div>
   );
 }
