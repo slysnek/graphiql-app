5a2ff8b46be9276efb0d6a175a24acce6f7e8635
Kirill <bloodwingest@gmail.com>
2023-05-27 07:33:41 +0300

    feat: add text box

--- GraphiQL-App/src/components/Documentation/DisplayTextBox.tsx
+++ GraphiQL-App/src/components/Documentation/DisplayTextBox.tsx
@@ -1,26 +1,44 @@
+import DisplayBox from './DisplayBox';
+
 interface DisplayTextBoxProps {
   header: string;
   noValue: string;
   displayType: string;
   localHistoryState: any;
+  allFields: any;
+  addToHistory: (element: any) => void;
 }
 
 export default function DisplayTextBox(props: DisplayTextBoxProps) {
-  /*   const historyState = useAppSelector((state) => state.historyState); */
-
   return (
     <div className="display-text-box">
       <h4>{props.header}</h4>
       <hr />
-      <ul>
-        {props.localHistoryState !== undefined &&
-        Object.hasOwn(props.localHistoryState, props.displayType) &&
-        props.localHistoryState[props.displayType] !== null ? (
-          <p>{props.localHistoryState[props.displayType]}</p>
-        ) : (
-          props.noValue
-        )}
-      </ul>
+      {/* description */}
+
+      {props.localHistoryState !== undefined &&
+      Object.hasOwn(props.localHistoryState, props.displayType) &&
+      props.localHistoryState[props.displayType] !== null &&
+      props.displayType === 'description' ? (
+        <p>{props.localHistoryState[props.displayType]}</p>
+      ) : props.localHistoryState !== undefined &&
+        props.displayType === 'metadata' &&
+        Object.hasOwn(props.localHistoryState, 'type') ? (
+        <div>
+          <p>Metadata for {props.localHistoryState.type.name}</p>
+          <DisplayBox
+            header="Fields"
+            noValue="No Fields"
+            displayType="fields"
+            addToHistory={props.addToHistory}
+            localHistoryState={props.allFields.find(
+              (el) => el.name === props.localHistoryState.type.name
+            )}
+          ></DisplayBox>
+        </div>
+      ) : (
+        props.noValue
+      )}
     </div>
   );
 }
