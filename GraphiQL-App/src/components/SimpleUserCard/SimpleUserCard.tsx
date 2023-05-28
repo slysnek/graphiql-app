746210a44619a0fe1f46c33723c15e837ef0c3e0
SergEnergy <kis8883244@gmail.com>
2023-05-28 22:30:44 +0300

    del: del unneccessary useCallback

--- GraphiQL-App/src/components/SimpleUserCard/SimpleUserCard.tsx
+++ GraphiQL-App/src/components/SimpleUserCard/SimpleUserCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import CardContent from './CardContent';
 import { CSSTransition } from 'react-transition-group';
 import { useTranslation } from 'react-i18next';
