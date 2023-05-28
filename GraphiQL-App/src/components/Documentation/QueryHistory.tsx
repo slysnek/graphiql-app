bfbb2ec44af1b9a0537a497753695f12a0202dda
Kirill <bloodwingest@gmail.com>
2023-05-28 03:19:45 +0300

    feat: add types and styles for query history

--- GraphiQL-App/src/components/Documentation/QueryHistory.tsx
+++ GraphiQL-App/src/components/Documentation/QueryHistory.tsx
@@ -1,15 +1,26 @@
-interface QueryHistoryProps {
-  currentHistory: string[];
-  historyReturn: () => void;
-}
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { QueryHistoryProps } from '../../types/interfaces';
+import './QueryHistory.css';
 
 export default function QueryHistory(props: QueryHistoryProps) {
   return (
-    <div>
-      <span onClick={props.historyReturn}>{` <<< `}</span>
-      {props.currentHistory.map((el, index) => {
-        return <span key={index}>--- {el} </span>;
-      })}
+    <div className="history-wrapper">
+      <ArrowBackIcon
+        className="back-button"
+        color="primary"
+        onClick={props.historyReturn}
+      ></ArrowBackIcon>
+      <div className="history">
+        {props.currentHistory.map((el, index) => {
+          return (
+            <div className="history-element" key={index}>
+              <NavigateNextIcon color="secondary"></NavigateNextIcon>
+              <b>{el}</b>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
